0e187ecddcf0e721d2e207c95ada2cb3cec35a87
John <john.serrano@usach.cl>
2022-11-08 22:45:31 -0300

    feat: Empleado Microservicio

---
+++ marcas-reloj-service/src/main/java/com/tingeso/marcasrelojservice/MarcasRelojServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tingeso.marcasrelojservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcasRelojServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcasRelojServiceApplication.class, args);
+	}
+
+}
